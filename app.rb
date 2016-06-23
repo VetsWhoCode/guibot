e0620e0482880e056e79a10f86e6f7d3a32d5e16
Jerome Hardaway <jeromedhardaway@gmail.com>
2016-06-22 20:21:50 -0500

    added commits

--- app.rb
+++ app.rb
@@ -1,22 +1,33 @@
+
+Paulo Abreu [14:39]
+added a Ruby snippet
 require 'sinatra'
 require 'httparty'
 require 'json'
-
+​
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
-
+​
   action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
-
+​
   case action
     when 'issues'
-      resp = HTTParty.get(repo_url)
-      resp = JSON.parse resp.body
+      resp = get_repository_details(repo_url)
       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+​
+    when 'commits'
+      resp = get_repository_details(repo_url)
+      respond_message "There are #{resp['commits_count']} commits on #{repo}"
   end
 end
-
+​
 def respond_message message
   content_type :json
   {:text => message}.to_json
+end
+​
+def get_repository_details(repo_url)
+  resp = HTTParty.get(repo_url)
+  JSON.parse resp.body
 end
