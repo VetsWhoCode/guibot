946855c9392cbdb17b0c3a5563b6420c1dcd9d1f
Jerome Hardaway <jeromedhardaway@gmail.com>
2016-06-22 20:27:07 -0500

    added commit code

--- app.rb
+++ app.rb
@@ -1,33 +1,27 @@
-
-Paulo Abreu [14:39]
-added a Ruby snippet
 require 'sinatra'
 require 'httparty'
 require 'json'
-​
+
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
-​
+
   action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
-​
+
   case action
     when 'issues'
-      resp = get_repository_details(repo_url)
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
-​
-    when 'commits'
-      resp = get_repository_details(repo_url)
-      respond_message "There are #{resp['commits_count']} commits on #{repo}"
+
+          when 'commits'
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
+      respond_message "There are #{resp['commits_count']} on #{repo}"
   end
 end
-​
+
 def respond_message message
   content_type :json
   {:text => message}.to_json
-end
-​
-def get_repository_details(repo_url)
-  resp = HTTParty.get(repo_url)
-  JSON.parse resp.body
 end
